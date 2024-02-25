ce01367357e2026c62dafcdeaf3614bd0d8373c4
rashmichau <rashmichaudhary2909@gmail.com>
2024-02-26 03:26:55 +0530

    Some basic concept of python

--- Basic1.py
+++ Basic1.py
@@ -12,6 +12,28 @@ print("current date and time")
 date_time= now.strftime("%Y-%m-%d ")
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
+ #find area of circle
+r = 4
+area = 3.14*r**2
+print(area)
+
+# first and last name and prints them in reverse order with a space between them
+fname ="Rashmi"
+lname = "Chaudhary"
+print("hello  ", lname ," " ,fname )
+
+# a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.
+"""variable = input("comma seprated numbers :")
+list = variable.split(",")
+tuple = tuple(list)
+print("list :",repr(list))
+print("tuple :",tuple)"""
+
+ #Python program to display the first and last colors from the following list.
+list = ["Red","Green","White" ,"Black"]
+print(list[0],list[-1])
+
+
 
 
 
