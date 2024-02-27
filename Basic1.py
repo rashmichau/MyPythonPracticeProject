586e758c63a4c15e2c618a2ae150100e04995e8f
rashmichau <rashmichaudhary2909@gmail.com>
2024-02-28 00:54:46 +0530

    Some basic concept of python

--- Basic1.py
+++ Basic1.py
@@ -28,23 +28,23 @@ import datetime
 # tuple = tuple(list)
 # print("list :",repr(list))
 # print("tuple :",tuple)"""
-#
-# # Python program to display the first and last colors from the following list.
+
+ # Python program to display the first and last colors from the following list.
 # list = ["Red", "Green", "White", "Black"]
 # print(list[0], list[-1])
-#
-# # Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+# Write a Python program to display the examination schedule. (extract the date from exam_st_date).
 # Examdate = 2, 5, 2024
 # print("the examination shedule :%i/ %i / %i" % Examdate)
-#
-# # # program that accepts an integer (n) and computes the value of n+nn+nnn.
+
+ # program that accepts an integer (n) and computes the value of n+nn+nnn.
 # # a = int(input("enter the value :"))
 # # n1 = int("%s" % a)
 # # n2 = int("%s%s" % (a, a))
 # # n3 = int("%s%s%s" % (a, a, a))
 # # print(n1 + n1 + n3)
-#
-# # printthedocuments(syntax, descriptionetc.) ofPythonbuilt - in function(s).
+
+# printthedocuments(syntax, descriptionetc.) ofPythonbuilt - in function(s).
 # print(abs.__doc__)
 #
 # # Write a Python program to get the volume of a sphere with radius six.
@@ -64,12 +64,53 @@ import datetime
 # else:
 #     print("no is smaller than 17 :",- (a - b))
 
-#program to test whether a not number is within 100 of 1000 or 2000
-a=int(input("any number:"))
-if a<=1100 and a>=900 in range(1100):
-    print('number within 100 of 1000 ')
-elif a<=2100 and a>=1900 in range (2000,) :
-    print('number  within 100 of 2000')
-else :
-    print("no is not in range of 1000 or 2000")
+# #program to test whether a not number is within 100 of 1000 or 2000
+# a=int(input("any number:"))
+# if a<=1100 and a>=900 in range(1100):
+#     print('number within 100 of 1000 ')
+# elif a<=2100 and a>=1900 in range (2000,) :
+#     print('number  within 100 of 2000')
+# else :
+#     print("no is not in range of 1000 or 2000")
 
+# #calculate the sum of three given numbers. If the values are equal, return three times their sum.
+# a,b,c = 4,4,4
+# d=int(3*(a+b+c))
+# if a==b==c:
+#     print("three times the no sum:",d)
+# else:
+#     print(a+b+c)
+
+# #program that returns a string that is n (non-negative integer) copies of a given string.
+# def large_string (test,n):
+#    result = " "
+#    for i in range(n):
+#     result = result+ test
+#    return result
+# print(large_string('abs',6))
+# print(large_string('.py',2))
+
+#program that determines whether a given number (accepted from the user) is even or odd, and prints an appropriate message to the user.
+# num = int(input("any number:"))
+# if num %2==0 :
+#     print("num is even")
+# else :
+#     print ("num is odd")
+
+#program to count the number 4 in a given list.
+# def count_num_4(nums):
+#     count = 0
+#     for num in nums :
+#         if num==4:
+#          count=count+1
+#     return count
+# print(count_num_4([4,5,4,7,4,8,9]))
+# print(count_num_4([4,5,4,4,4,4,4,5]))
+
+# #program to test whether a passed letter is a vowel or not.
+# def letter_is_vowel(letter):
+#    vowel = 'aeiou'
+#    return letter in vowel
+#
+# print(letter_is_vowel('a'))
+# print(letter_is_vowel('6futa'))
