b560579339f92ab2b535a7b187dc10503490063a
rashmichau <rashmichaudhary2909@gmail.com>
2024-07-26 00:04:49 +0530

    Some basic concept of python

---
+++ code for three month.py
@@ -0,0 +1,17 @@
+# import time
+# timetemp = time.strftime('%H:%M:%S')
+# print(timetemp)
+#
+# colours = ['RED','BLUE','YELLOW','BLACK']
+# for colour in colours:
+#  print(colour)
+#  for i in colour:
+#     print(i)
+
+# for i in range(0,18):
+#     print(i+1)
+
+i=int(input('enter the number:'))
+while (i<3):
+    i=int(input('enter the num:'))
+    print(i)
