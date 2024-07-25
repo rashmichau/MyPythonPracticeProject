6274eb1612ee030eda3cfcc2e0ab5fd00b8579e3
rashmichau <rashmichaudhary2909@gmail.com>
2024-07-26 00:04:49 +0530

    Some basic concept of python

--- ReverseString.py
+++ ReverseString.py
@@ -1,8 +1,56 @@
-x= input("enter first digigt of code :")
-y= input("enter second digit of code :")
-print(int(x)+int(y))
-print(int(x)/int(y))
-print(int(x)*int(y))
-print(int(x)//int(y))
-print(int(x)%int(y))
-print(int(x)**2)
+# x= input("enter first digigt of code :")
+# y= input("enter second digit of code :")
+# print(int(x)+int(y))
+# print(int(x)/int(y))
+# print(int(x)*int(y))
+# print(int(x)//int(y))
+# print(int(x)%int(y))
+# print(int(x)**2)
+
+# nm ="Harry"
+# print(nm[-4:-2])
+# print(nm[ : :-1])
+
+#eligible for votre
+# age = int(input("Enter age  of candidate :"))
+# if (age >= 18):
+#    print("candidate are eligible for vote")
+# else:
+#     print("candidate are not eligible for vote")
+
+# Digit is even or odd
+# num = int(input("Enter a digit : "))
+# if (num%2==0):
+#     print("entered digit is even")
+# else:
+#     print("entered digit is odd")
+
+# Number is devisible by seven or not
+# num = int(input("Enter a number :"))
+# if (num % 7==0):
+#     print("given num is divisible by seven")
+# else:
+#     print("given no is not divisible by seven")
+
+# user is a multiple of five
+# user = int(input("Enter a num :"))
+# if (user%5==0 ):
+#     print("Hello")
+# else :
+#     print("Bye")
+
+#unit charge
+# unit = int(input("Enter no of units :"))
+# if unit<=100:
+#     print ("no charge")
+# elif (unit>100 and unit<=200):
+#     print("unit charge is :",(unit-100)*5)
+# else:
+#     print("unit charge is :",500+(unit-200)*10)
+
+# num = int(input("Enter any num :"))
+# a= num%10
+# if (a%3==0):
+#     print("yes it is divisible by 3")
+# else:
+#     print("No")
